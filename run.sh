bcc510c22691f628d008d92fe019f543a634648d
levent yıldız <lewenty@gmail.com>
2018-04-23 12:41:48 +0300

    set default slave container count

--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
-echo "$1 slave and 1 master  container will be created..."
+#set default slave container count if its unset
+if [ -z "$1" ]
+  then
+    slaveCount=1
+  else
+  	slaveCount=$1
+fi
+
+echo "$slaveCount slave and 1 master  container will be created..."
 
 #add all slave containers name  to slaves file
-for (( i=1; i<=$1; i++ ))
+for (( i=1; i<=$slaveCount; i++ ))
 do
     echo "Exporting slave$i to salves file..."
     echo "slave$i" >> ./conf/slaves
 done
 
 #Create a network named "hadoopNetwork"
-docker network create -d bridge   --subnet 172.25.0.0/16  hadoopNetwork
+docker network rm hadoopNetwork && docker network create -d bridge   --subnet 172.25.0.0/16  hadoopNetwork
 
 #Create base hadoop image named "base-hadoop:1.0"
 docker build -t base-hadoop:1.0 .
@@ -17,7 +25,7 @@ docker build -t base-hadoop:1.0 .
 docker run -itd  --network="hadoopNetwork"  --ip 172.25.0.100  -p 50070:50070  -p 8088:8088 --name master --hostname master  base-hadoop:1.0
 
 
-for (( c=1; c<=$1; c++ ))
+for (( c=1; c<=$slaveCount; c++ ))
 do
     tmpName="slave$c"
     #run base-hadoop:1.0 image  as slave container
